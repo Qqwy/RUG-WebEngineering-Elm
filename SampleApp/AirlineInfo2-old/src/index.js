58fec4b7cf555e3b79db7a858356b41a45bf0eb6
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-03-12 22:45:43 +0100

    remove old stuff

--- SampleApp/AirlineInfo2-old/src/index.js
+++
@@ -1,8 +0,0 @@
-// Styles
-require('./assets/styles/main.scss');
-
-// Vendor JS is imported as an entry in webpack.config.js
-
-// Elm
-var Elm = require('./elm/Main.elm').Elm;
-Elm.Main.init({node: document.body});
