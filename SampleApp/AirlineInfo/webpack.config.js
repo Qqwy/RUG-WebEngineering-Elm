f8e24d6909b8d1909cf0c1cd40fb237d217950b9
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-03-12 23:01:21 +0100

    Updates to config to build properly for production

--- SampleApp/AirlineInfo/webpack.config.js
+++ SampleApp/AirlineInfo/webpack.config.js
@@ -1,15 +1,19 @@
+const webpack = require('webpack');
 const path = require('path');
 const webpackMerge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-const StyleLintPlugin = require('stylelint-webpack-plugin');
+// const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
 const presetConfig = require("./build-utils/loadPresets");
 
-module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
+module.exports = ({ mode, presets, asset_path} = { mode: "production", presets: [], asset_path: "/"}) => {
   console.log(`Building for: ${mode}`);
 
+// Try the environment variable, otherwise use root
+console.log('asset_path', asset_path); // 'local'
+// __webpack_public_path__ = asset_path;
   return webpackMerge(
     {
       mode,
@@ -18,6 +22,38 @@ module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
         main: path.join(__dirname, './src/index.js'),
         vendor: path.join(__dirname, './src/assets/js/vendor.js'),
       },
+        output: {
+            publicPath: asset_path
+        },
+        devServer: {
+            disableHostCheck: true,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                    use: [{
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[hash].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }]
+                },
+                {
+                    test: /\.(png|jpg|gif|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                    use: [{
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[hash].[ext]',
+                            outputPath:  'images/'
+                        }
+                    }
+                    ]
+                }
+
+            ]
+        },
 
       plugins: [
         new HtmlWebpackPlugin({
